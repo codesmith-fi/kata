dbe44bcd2543ea5cc6adf168a9add247dcdfcb73
Erno Pakarinen <erpakari@gmail.com>
2019-03-05 18:09:19 +0200

    isFizz and isBuzz methods now check if the given number contains 3 or 5

--- fizzbuzz/src/fizzbuzz/FizzBuzzEvaluator.java
+++ fizzbuzz/src/fizzbuzz/FizzBuzzEvaluator.java
@@ -13,23 +13,23 @@ public class FizzBuzzEvaluator {
 	
 	/**
 	 * Method to evaluate if the given number is Fizz
-	 * meaning it's divisible by three
+	 * meaning it's divisible by three or contains "3"
 	 * @param number string of a number to be tested
 	 * @return true if the number is Fizz
 	 */
 	public static boolean isFizz(String number) {
 		int val = Integer.parseInt(number);
-		return (val % 3 == 0) && (val != 0);
+		return number.contains("3") || (val % 3 == 0) && (val != 0);
 	}
 	
 	/**
 	 * Method to evaluate if the given number is Buzz
-	 * meaning it's divisible by five
+	 * meaning it's divisible by five or contains "5"
 	 * @param number string of a number to be tested
 	 * @return true if the number is Buzz
 	 */
 	public static boolean isBuzz(String number) {
 		int val = Integer.parseInt(number);
-		return (val % 5 == 0) && (val != 0);
+		return number.contains("5") || (val % 5 == 0) && (val != 0);
 	}
 }
