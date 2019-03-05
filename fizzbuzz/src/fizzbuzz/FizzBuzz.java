3fd8a496c455399dc4587aba21cadad14a9c7763
Erno Pakarinen <erpakari@gmail.com>
2019-03-05 18:46:30 +0200

    Cleanup, handles the given argument, tests updated for new rules

--- fizzbuzz/src/fizzbuzz/FizzBuzz.java
+++ fizzbuzz/src/fizzbuzz/FizzBuzz.java
@@ -4,6 +4,9 @@
 package fizzbuzz;	
 
 /**
+ * FizzBuzz kata implementation, application entry point with needed testing
+ * logic
+ * @see http://codingdojo.org/kata/FizzBuzz/ 
  * @author erno
  *
  */
@@ -13,41 +16,49 @@ public final class FizzBuzz {
 	 * Program entry point for the FizzBuzz application. Displays output in 
 	 * the console window. Evaluates a series of numbers to see if the 
 	 * number is either Fizz or Buzz depending on if the number is divisible
-	 * by three or five. If neither, the number itself is displayed.
+	 * by three or five or contains "3" or "5". 
+	 * If neither, the number itself is displayed.
 	 * 
 	 * @param args If a number is given the app displays the following
-	 *		Fizz, the number is divisible by three
-	 *		Buzz, the number is divisible by five
+	 *		Fizz, the number is divisible by three or contains "3"
+	 *		Buzz, the number is divisible by five or contains "5"
 	 *		the number itself, none of the above
 	 *		If no argument, displays Fizz/Buzz evaluation for numbers 1-100
-	 *@return String "Fizz", "Buzz" or the number itself if argument was given
 	 */
 	public static void main(String[] args) {
-		// Todo: Arguments not yet implemented 
+		// If argument was provided, the first arg will be checked
 		if(args.length > 0) {
-			throw new IllegalArgumentException("Arguments not yet implemented!");
-		}
-		
-		// Go though numbers from 1 to 100 and print out either "Fizz", "Buzz", "FizzBuzz"
-		// or the number itself depending on if the number is divisible by three, five 
-		// or neither
-		System.out.printf("Checking numbers from 1 to 100 for Fizz/Buzz:%n");
-		for(int i = 1; i <= 100; i++) {
-			String value = String.valueOf(i);
-			FizzBuzz.checkString(value);
+			FizzBuzz.checkString(args[0]);			
+		} else {
+			// Go though numbers from 1 to 100 and print out either "Fizz", "Buzz", "FizzBuzz"
+			// or the number itself depending on if the number is divisible by three, five 
+			// or neither
+			System.out.printf("Checking numbers from 1 to 100 for Fizz/Buzz:%n");
+			for(int i = 1; i <= 100; i++) {
+				String value = String.valueOf(i);
+				FizzBuzz.checkString(value);
+			}
 		}
 	}
 	
+	/**
+	 * Helper method to do the actual Fizz, Buzz testing for a given string
+	 * @param valueStr number string to be tested
+	 */
 	public static void checkString(String valueStr) {
-		if(FizzBuzzEvaluator.isFizz(valueStr)) {
-			System.out.print("Fizz");
+		try {
+			if(FizzBuzzEvaluator.isFizz(valueStr)) {
+				System.out.print("Fizz");
+			}
+			if(FizzBuzzEvaluator.isBuzz(valueStr)) {
+				System.out.print("Buzz");
+			}
+			if(!FizzBuzzEvaluator.isFizz(valueStr) && !FizzBuzzEvaluator.isBuzz(valueStr)) {
+				System.out.print(valueStr);		
+			}
+			System.out.println("");
+		} catch(NumberFormatException e) {
+			System.out.printf("String %s is not a valid number!%n", valueStr);
 		}
-		if(FizzBuzzEvaluator.isBuzz(valueStr)) {
-			System.out.print("Buzz");
-		}
-		if(!FizzBuzzEvaluator.isFizz(valueStr) && !FizzBuzzEvaluator.isBuzz(valueStr)) {
-			System.out.print(i);		
-		}
-		System.out.println("");		
 	}
 }
--- fizzbuzz/srcTest/fizzbuzzTest/FizzBuzzEvaluatorTest.java
+++ fizzbuzz/srcTest/fizzbuzzTest/FizzBuzzEvaluatorTest.java
@@ -32,9 +32,9 @@ class FizzBuzzEvaluatorTest {
 	/**
 	 * Test isFizz method with a valid Fizz number, divisible by three
 	 */
-	@DisplayName("Number is divisable by three (isFizz returns true)")
-	@ParameterizedTest(name ="Number {0} is divisable by three (Fizz)")
-	@ValueSource(strings = {"3", "6", "9", "12", "15", "18", "21", "24"})
+	@DisplayName("Number is divisable by three or contains 3 (isFizz returns true)")
+	@ParameterizedTest(name ="Number {0} is Fizz")
+	@ValueSource(strings = {"3", "6", "9", "12", "13", "15", "18", "21", "24", "23", "33"})
 	void testIsFizzValid(String number) {
 		assertTrue(FizzBuzzEvaluator.isFizz(number));
 	}
@@ -42,9 +42,9 @@ class FizzBuzzEvaluatorTest {
 	/**
 	 * Test isFizz method with invalid Fizz number, not divisible by three
 	 */
-	@DisplayName("Number is not divisable by three (isFizz returns false)")
-	@ParameterizedTest(name ="Number {0} is not divisable by three (Fizz)")
-	@ValueSource(strings = {"0", "1", "2", "4", "5", "7", "8", "10", "11", "13"})
+	@DisplayName("Number is not divisable by three or contain 5 (isFizz returns false)")
+	@ParameterizedTest(name ="Number {0} is not Fizz")
+	@ValueSource(strings = {"0", "1", "2", "4", "5", "7", "8", "10", "11", "14", "16"})
 	void testIsInFizzValid(String number) {
 		assertFalse(FizzBuzzEvaluator.isFizz(number));
 	}	
@@ -52,9 +52,9 @@ class FizzBuzzEvaluatorTest {
 	/**
 	 * Test isBuzz method with a valid Buzz number, divisible by five
 	 */
-	@DisplayName("Number is divisable by five (isBuzz returns true)")
-	@ParameterizedTest(name ="Number {0} is divisable by five (Buzz)")
-	@ValueSource(strings = {"5", "10", "15", "20", "25", "30", "35", "40"})
+	@DisplayName("Number is divisable by five or contains 5 (isBuzz returns true)")
+	@ParameterizedTest(name ="Number {0} is Buzz")
+	@ValueSource(strings = {"5", "10", "15", "20", "25", "30", "35", "40", "155"})
 	void testIsBuzzValid(String number) {
 		assertTrue(FizzBuzzEvaluator.isBuzz(number));
 	}
@@ -62,9 +62,9 @@ class FizzBuzzEvaluatorTest {
 	/**
 	 * Test isBuzz method with a invalid Buzz number, not divisible by five
 	 */
-	@DisplayName("Number is not divisable by five (isBuzz returns false)")
-	@ParameterizedTest(name ="Number {0} is not divisable by five (Buzz)")
-	@ValueSource(strings = {"0", "1", "2", "3", "4", "6", "7", "8", "9", "11"})
+	@DisplayName("Number is not divisable by five or contain 5 (isBuzz returns false)")
+	@ParameterizedTest(name ="Number {0} is not Buzz")
+	@ValueSource(strings = {"0", "1", "2", "3", "4", "6", "7", "8", "9", "11", "13", "23"})
 	void testIsBuzzInvalid(String number) {
 		assertFalse(FizzBuzzEvaluator.isBuzz(number));
 	}
